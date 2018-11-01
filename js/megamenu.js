68006f4902a37a97ce1fa592d00300e68d48bf0f
Andrey Kolomoets <kolomoets.and@gmail.com>
2018-11-01 14:17:10 +0200

    items & category

--- js/megamenu.js
+++ js/megamenu.js
@@ -59,3 +59,31 @@ $(document).ready(function () {
     //when clicked on mobile-menu, normal menu is shown as a list, classic rwd menu story
 
 });
+
+
+
+// Plus-Minus
+// Min Max значения изменяются в HTML -> item.html -> input-group -> input type="text" class="form-control" value="1" min="0" max="100"
+
+$(function(){
+
+    $('.spinner .btn:first-of-type').on('click', function() {
+      var btn = $(this);
+      var input = btn.closest('.spinner').find('input');
+      if (input.attr('max') == undefined || parseInt(input.val()) < parseInt(input.attr('max'))) {    
+        input.val(parseInt(input.val(), 10) + 1);
+      } else {
+        btn.next("disabled", true);
+      }
+    });
+    $('.spinner .btn:last-of-type').on('click', function() {
+      var btn = $(this);
+      var input = btn.closest('.spinner').find('input');
+      if (input.attr('min') == undefined || parseInt(input.val()) > parseInt(input.attr('min'))) {    
+        input.val(parseInt(input.val(), 10) - 1);
+      } else {
+        btn.prev("disabled", true);
+      }
+    });
+
+});
