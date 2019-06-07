3b9dbc33c87f8d55a92fe1fee8ed7e667a86baa1
Andrey Kolomoets <kolomoets.and@gmail.com>
2019-06-07 12:31:22 +0300

    mobile logo to index page

--- js/megamenu.js
+++ js/megamenu.js
@@ -10,8 +10,11 @@ $(document).ready(function () {
     $('.menu > ul > li > ul:not(:has(ul))').addClass('normal-sub');
     //Checks if dropdown menu's li elements have anothere level (ul), if not the dropdown is shown as regular dropdown, not a mega menu
 
-    $(".menu > ul").before("<a class=\"menu-mobile\" href=\"#\"><img class=\"navbar_logo\" src=\"images/logo-mobile.png\" alt=\"FitClothing\"></a>");
-        
+    //$(".menu > ul").before("<a class=\"menu-mobile\" href=\"index.html\"><img class=\"navbar_logo\" src=\"images/logo-mobile.png\" alt=\"FitClothing\"></a>");
+    $(".menu > ul").before("<div class=\"mobile_block\"><a class=\"logo_mobiled\" href=\"index.html\"><img class=\"navbar_logo\" src=\"images/logo-mobile.png\" alt=\"FitClothing\"></a>" + "<a class=\"menu-mobile\" href=\"index.html\"></a></div>");
+
+    //$(".menu > .menu-mobile").before("<a class=\"logo_mobiled\" href=\"index.html\"><img class=\"navbar_logo\" src=\"images/logo-mobile.png\" alt=\"FitClothing\"></a>");
+
     //Adds menu-mobile class (for mobile toggle menu) before the normal menu
     //Mobile menu is hidden if width is more then 959px, but normal menu is displayed
     //Normal menu is hidden if width is below 959px, and jquery adds mobile menu
